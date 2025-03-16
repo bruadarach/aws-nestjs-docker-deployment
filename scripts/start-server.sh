444a8e94696966e6dcdd1c67e700733fd69519eb
Sujeong Ji <suji.sujeongji@gmail.com>
2025-03-16 20:19:41 +0000

    feat: deploy Docker image using ECR, S3, Code Deploy and EC2 on AWS

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+# echo "--------------- 서버 배포 시작 -----------------"
+# cd /home/ubuntu/linond-server
+# npm i
+# pm2 kill
+# pm2 start dist/main.js --name "backend-server"
+# echo "--------------- 서버 배포 끝 -----------------"
+
+
 echo "--------------- 서버 배포 시작 -----------------"
-cd /home/ubuntu/linond-server
-npm i
-pm2 kill
-pm2 start dist/main.js --name "backend-server"
-echo "--------------- 서버 배포 끝 -----------------"
+docker stop linond-server || true
+docker rm linond-server || true
+docker pull 248189943624.dkr.ecr.eu-west-2.amazonaws.com/linond-server:latest # ECR 주소
+docker run -d --name linond-server -p 3000:3000 248189943624.dkr.ecr.eu-west-2.amazonaws.com/linond-server:latest # ECR 주소
+echo "--------------- 서버 배포 끝 ------------------"
